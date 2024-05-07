5b22dd6fa81aed6f39113ed57e6619ff0ba5d338
Saurabh Mishra <saurabhmishra1464@gmail.com>
2024-05-07 17:13:09 +0530

    fetchUsingXHRAndAsyncAwait

--- script.js
+++ script.js
@@ -29,10 +29,6 @@ function displayResponse(posts) {
     ).join(" ");
 }
 
-// fetchUsingXHR();
-// fetchUsingFetchMethod();
-fetchUsingAsyncAwaitMethod();
-
 function fetchUsingFetchMethod() {
     debugger
     const fetchRequest = fetch('https://jsonplaceholder.typicode.com/todos', {
@@ -41,10 +37,42 @@ function fetchUsingFetchMethod() {
     fetchRequest.then(response => response.json()).then(result => displayResponse(result)).catch(e => console.log(e));
 }
 
-async function fetchUsingAsyncAwaitMethod(){
+async function fetchUsingAsyncAwaitMethod() {
     const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
         method: 'GET',
     })
     const result = await response.json();
     displayResponse(result);
 }
+
+function helperMethod(method, url) {
+    debugger
+    const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.responseType = 'json';
+        xhr.send();
+
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                resolve(xhr.response)
+            } else {
+                reject(xhr.response)
+            }
+        }
+    })
+    return promise;
+}
+
+async function fetchUsingXHRAndAsyncAwait() {
+    const response = await helperMethod('GET', "https://jsonplaceholder.typicode.com/todos");
+    console.log("=========================");
+    console.log(response);
+    console.log("=========================");
+    displayResponse(response);
+}
+
+// fetchUsingXHR();
+// fetchUsingFetchMethod();
+// fetchUsingAsyncAwaitMethod();
+fetchUsingXHRAndAsyncAwait();
